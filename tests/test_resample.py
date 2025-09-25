14e952c48a4de244e31b290059a6b7fa13e6d962
Chris Wehmeyer <chris@wehmeyer.co.uk>
2025-09-25 21:47:04 +0200

    Added Dirichlet resampler and tests.

---
+++ tests/test_resample.py
@@ -0,0 +1,38 @@
+import pytest
+
+from bbstat.resample import Resample
+import numpy as np
+from typing import Optional
+
+
+@pytest.mark.parametrize(
+    "data_size",
+    [
+        pytest.param(2),
+        pytest.param(5),
+    ],
+)
+@pytest.mark.parametrize(
+    "n_samples",
+    [
+        pytest.param(1),
+        pytest.param(3),
+    ],
+)
+@pytest.mark.parametrize(
+    "blocksize",
+    [
+        pytest.param(None),
+        pytest.param(1),
+        pytest.param(10),
+    ],
+)
+def test_resample(data_size: int, n_samples: int, blocksize: Optional[int]) -> None:
+    resample = Resample(alpha=np.ones(shape=(data_size,), dtype=np.float64))
+    samples = list(resample(n_samples))
+    assert len(samples) == n_samples
+    for sample in samples:
+        assert sample.shape == (data_size,)
+        assert (0 <= sample).all()
+        assert (sample <= 1).all()
+        np.testing.assert_allclose(np.sum(sample), 1.0)
