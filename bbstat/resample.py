2a9a35aad05d73890d4912800d90f0f1244394db
Chris Wehmeyer <chris@wehmeyer.co.uk>
2025-10-08 16:15:16 +0200

    Added resample function to replace dataclass.

--- bbstat/resample.py
+++ bbstat/resample.py
@@ -25,3 +25,43 @@ class Resample:
     ) -> "Resample":
         alpha = np.ones(shape=(len(data),), dtype=data.dtype)
         return cls(alpha=alpha, blocksize=blocksize)
+
+
+def resample(
+    n_boot: int,
+    n_data: int,
+    seed: Optional[int] = None,
+    blocksize: Optional[int] = None,
+) -> Generator[NDArray[np.floating], None, None]:
+    """Generate Dirichlet-distributed weights.
+
+    Parameters
+    ----------
+    n_boot: int
+        Number of resampling steps for the bootstrap.
+    n_data: int
+        Number of data points to resample.
+    seed: Optional[int], default is None
+        Optional seed for the random number generator.
+    blocksize: Optional[int], default is None
+        Optional size for the `numpy.random.Generator.dirichlet()` call
+        (how many resampling steps are generated in one call).
+
+    Returns
+    -------
+    Generator[numpy.array(shape=n_data), None, None]
+        Dirichlet-distributed weights with `alpha=numpy.ones(n_data)`.
+    """
+    rng = np.random.default_rng(seed)
+    alpha = np.ones(n_data)
+    if blocksize is None:
+        blocksize = n_boot
+    else:
+        blocksize = min(blocksize, n_boot)
+    remainder = n_boot
+    while remainder > 0:
+        size = min(blocksize, remainder)
+        weights = rng.dirichlet(alpha=alpha, size=size)
+        for w in weights:
+            yield w
+        remainder -= size
--- tests/test_resample.py
+++ tests/test_resample.py
@@ -3,7 +3,7 @@ from typing import Optional
 import numpy as np
 import pytest
 
-from bbstat.resample import Resample
+from bbstat.resample import Resample, resample
 
 
 @pytest.mark.parametrize(
@@ -28,12 +28,63 @@ from bbstat.resample import Resample
         pytest.param(10),
     ],
 )
-def test_resample(data_size: int, n_samples: int, blocksize: Optional[int]) -> None:
-    resample = Resample(alpha=np.ones(shape=(data_size,), dtype=np.float64))
-    samples = list(resample(n_samples))
+def test_Resample(data_size: int, n_samples: int, blocksize: Optional[int]) -> None:
+    resampler = Resample(alpha=np.ones(shape=(data_size,), dtype=np.float64))
+    samples = list(resampler(n_samples))
     assert len(samples) == n_samples
     for sample in samples:
         assert sample.shape == (data_size,)
+        assert (0 <= sample).all()
+        assert (sample <= 1).all()
+        np.testing.assert_allclose(np.sum(sample), 1.0)
+
+
+@pytest.mark.parametrize(
+    "n_boot",
+    [
+        pytest.param(1),
+        pytest.param(5),
+    ],
+)
+@pytest.mark.parametrize(
+    "n_data",
+    [
+        pytest.param(2),
+        pytest.param(3),
+    ],
+)
+@pytest.mark.parametrize(
+    "seed",
+    [
+        pytest.param(None),
+        pytest.param(1),
+    ],
+)
+@pytest.mark.parametrize(
+    "blocksize",
+    [
+        pytest.param(None),
+        pytest.param(1),
+        pytest.param(10),
+    ],
+)
+def test_resample(
+    n_boot: int,
+    n_data: int,
+    seed: Optional[int],
+    blocksize: Optional[int],
+) -> None:
+    samples = list(
+        resample(
+            n_boot=n_boot,
+            n_data=n_data,
+            seed=seed,
+            blocksize=blocksize,
+        )
+    )
+    assert len(samples) == n_boot
+    for sample in samples:
+        assert sample.shape == (n_data,)
         assert (0 <= sample).all()
         assert (sample <= 1).all()
         np.testing.assert_allclose(np.sum(sample), 1.0)
