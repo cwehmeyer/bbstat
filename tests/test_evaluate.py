86cfc945d6ba382e3dcff22b65a6e300d77df461
Chris Wehmeyer <chris@wehmeyer.co.uk>
2025-10-27 12:34:56 +0100

    Added tests for plotting results.

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,5 +1,7 @@
-from typing import Tuple
+from typing import Optional, Tuple, cast
 
+import matplotlib.collections as mcoll
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 from numpy.typing import NDArray
@@ -10,6 +12,11 @@ from bbstat.evaluate import BootstrapResult, credibility_interval
 @pytest.fixture(scope="module")
 def estimates() -> NDArray[np.floating]:
     return np.linspace(0, 1, 101)
+
+
+@pytest.fixture(scope="module")
+def bootstrap_result(estimates) -> BootstrapResult:
+    return BootstrapResult(estimates=estimates, coverage=0.95)
 
 
 @pytest.mark.parametrize(
@@ -132,3 +139,52 @@ def test_bootstrap_result_str() -> None:
 def test_bootstrap_result_ndigits(ci: Tuple[float, float], expected: int) -> None:
     actual = BootstrapResult.ndigits(ci=ci)
     assert actual == expected
+
+
+def test_plot_returns_axes(bootstrap_result: BootstrapResult) -> None:
+    ax = bootstrap_result.plot()
+    assert isinstance(ax, plt.Axes)
+
+
+@pytest.mark.parametrize(
+    "coverage, expected_title",
+    [
+        pytest.param(None, "Bayesian bootstrap  •  101 resamples, 95% CI"),
+        pytest.param(0.99, "Bayesian bootstrap  •  101 resamples, 99% CI"),
+    ],
+)
+def test_plot_respects_coverage_in_title(
+    bootstrap_result: BootstrapResult,
+    coverage: Optional[float],
+    expected_title: str,
+) -> None:
+    ax = bootstrap_result.plot(coverage=coverage)
+    actual_title = ax.get_title()
+    assert isinstance(actual_title, str)
+    assert actual_title == expected_title
+
+
+def test_plot_adds_three_lines_and_one_fill(bootstrap_result: BootstrapResult) -> None:
+    fig, ax = plt.subplots()
+    _ = bootstrap_result.plot(ax=ax)
+    assert len(ax.lines) == 3
+    assert len([c for c in ax.collections if isinstance(c, mcoll.PolyCollection)]) == 1
+
+
+@pytest.mark.parametrize(
+    "label, expected_label",
+    [
+        pytest.param(None, "0.499"),
+        pytest.param("my_stat", "my_stat=0.499"),
+    ],
+)
+def test_plot_labels_match(
+    bootstrap_result: BootstrapResult,
+    label: Optional[str],
+    expected_label: str,
+) -> None:
+    label = "my_stat"
+    ax = bootstrap_result.plot(label=label)
+    actual_label = ax.lines[0].get_label()
+    assert isinstance(label, str)
+    cast(str, actual_label).startswith(expected_label)
